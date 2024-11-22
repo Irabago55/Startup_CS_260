d7332b9ff757146fa24569cb030302918365c901
Isaac <irabago@byu.edu>
2024-11-22 12:13:10 -0700

    added the new client code for my website to work on database.js

--- service/database.js
+++ service/database.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-const client = new MongoClient(url);
+const client = new MongoClient(url, { tls: true, serverSelectionTimeoutMS: 3000, autoSelectFamily: false, });
 const db = client.db('startup');
 const userCollection = db.collection('user');
 const cartCollection = db.collection('cart');
